26c7f9700605810f20a7bb26ce49a8b0f13a7d65
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-10-11 01:20:40 +0100

    staring up on the home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import "./style.scss"
+
 
 function App() {
   return (
     <div className="App">
-        <Register />
-    {/* <Login />  */}
+        <Home />
     </div>
   );
 }
