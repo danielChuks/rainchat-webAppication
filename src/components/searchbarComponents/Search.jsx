338bb3751c0b6d64183e45116755fb9b48bc34b8
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-10-23 02:08:25 +0100

    finding users

--- src/components/searchbarComponents/Search.jsx
+++ src/components/searchbarComponents/Search.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Search() {
+  const [username, setUsername] = useState("")
+  const [user, setUser]= useState(null);
+  const [error, setError]= useState(false);
+
+
+  const handleSearch = () =>{
+    
+  }
+
+  const handleKey = (e) =>{
+    e.code === "Enter" && handleSearch()
+  }
+
   return (
     <div className='search'>
       <div className="searchForm">
-          <input type="text" placeholder='Find a user'/>
+          <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)}/>
       </div>
       <div className="userChat">
         <img src="https://images.pexels.com/photos/9968415/pexels-photo-9968415.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"  alt=""/>
