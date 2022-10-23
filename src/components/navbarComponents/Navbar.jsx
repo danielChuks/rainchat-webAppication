6a6af6d2533a6cfd9887749acf62017a5107126c
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-10-23 01:56:13 +0100

    dynamic user display

--- src/components/navbarComponents/Navbar.jsx
+++ src/components/navbarComponents/Navbar.jsx
@@ -1,14 +1,17 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 import { auth } from '../../firebase'
 
+
 function Navbar() {
+const {currentUser} = useContext(AuthContext);
   return (
     <div className='navbar'>
       <span className='logo'> Rain Chat</span>
         <div className="user">
-          <img src="https://images.pexels.com/photos/9968415/pexels-photo-9968415.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=''/>
-            <span>Daniel</span>
+          <img src={currentUser.photoURL} alt=''/>
+            <span>{currentUser.displayName}</span>
             <button onClick={ () => signOut(auth)}>Logout</button>
         </div>
     </div>
