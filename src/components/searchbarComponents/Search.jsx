785e4d2b105315e4a19f562ab7c6305a136d2ca1
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2022-10-26 13:49:13 +0100

    code base

--- src/components/searchbarComponents/Search.jsx
+++ src/components/searchbarComponents/Search.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useContext, useState } from 'react'
+import { collection, query, where, getDocs, setDoc } from "firebase/firestore";
 import { db } from '../../firebase';
+import { AuthContext } from '../../context/AuthContext';
 
 
 function Search() {
   const [username, setUsername] = useState("")
   const [user, setUser]= useState(null);
   const [error, setError]= useState(false);
+  const {currentUser} = useContext(AuthContext)
 
 
 
@@ -19,7 +21,6 @@ function Search() {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         setUser(doc.data())
-        console.log(user)
      });
     } catch (error) {
       setError(true);
@@ -32,17 +33,37 @@ function Search() {
     e.code === "Enter" && handleSearch();
   }
 
-  return (
+
+  const handleSelect = async() => {
+    //we check if the group (chats in firestore) exist if not we create.
+    const combinedId = currentUser.uid > user.uid 
+      ? currentUser.uid + user.uid
+      : user.uid + currentUser.uid
+
+      try {
+        const res = await getDocs(db, "chats", combinedId)
+        if(!res.exist()){
+          //create a chat in the chat collection
+          await setDoc((db, "chats", combinedId), {messages:[]})
+        }
+      } catch (error) {
+        
+      }
+  }
+
+  return ( 
     <div className='search'>
       <div className="searchForm">
-          <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)}/>
+          <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={(e)=> setUsername(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src={user.photoURL}  alt=""/>
+
+      {error && <span>User not found!</span>}
+      {user && <div className="userChat" onClick={handleSelect}>
+        <img src={user.photoURL} alt=""/>
           <div className="userinfo">
             <span>{user.displayName}</span>
           </div>
-      </div>
+      </div> }
     </div>
   )
 }
